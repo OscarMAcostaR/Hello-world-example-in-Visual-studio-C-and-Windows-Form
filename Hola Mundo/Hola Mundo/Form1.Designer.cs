982405cab36c2b998aeb5735f42625e2c43d3a53
Oscar Allstar <oscar96261@gmail.com>
2021-04-17 16:44:21 -0500

    Proyecto hola Mundo en C# y Windows Form

---
+++ Hola Mundo/Hola Mundo/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Hola_Mundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSaludar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnSaludar
+            // 
+            this.BtnSaludar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSaludar.Location = new System.Drawing.Point(82, 60);
+            this.BtnSaludar.Name = "BtnSaludar";
+            this.BtnSaludar.Size = new System.Drawing.Size(115, 34);
+            this.BtnSaludar.TabIndex = 0;
+            this.BtnSaludar.Text = "Saludar";
+            this.BtnSaludar.UseVisualStyleBackColor = true;
+            this.BtnSaludar.Click += new System.EventHandler(this.BtnSaludar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 150);
+            this.Controls.Add(this.BtnSaludar);
+            this.Name = "Form1";
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSaludar;
+    }
+}
+
